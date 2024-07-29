30f87cfec8f4a7840321aa2ddc521cc141889f71
Risa Yamamoto <yamari2422@gmail.com>
2024-07-29 16:40:26 -0700

    finished exercise

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
-$(function() {
+$(function () {
   // YOUR CODE HERE
-})
+  const button = $("#toggleButton");
+  const paragraph = $("#myParagraph");
+
+  const colorButton = $("#colorButton");
+  const colorParagraph = $("#colorDiv");
+
+  const addButton = $("#addClassButton");
+  const removeButton = $("#removeClassButton");
+  const classDiv = $("#classDiv");
+
+  const fadeInButton = $("#fadeInButton");
+  const fadeOutButton = $("#fadeOutButton");
+  const fadeDiv = $("#fadeDiv");
+
+  const slideUpButton = $("#slideUpButton");
+  const slideDownButton = $("#slideDownButton");
+  const slideDiv = $("#slideDiv");
+
+  // Exercise 1
+  button.on("click", function () {
+    paragraph.toggle();
+  });
+
+  // Exercise 2
+  colorButton.on("click", function () {
+    colorParagraph.css("backgroundColor", "yellow");
+  });
+
+  // Exercise 3
+  addButton.on("click", function () {
+    classDiv.addClass("new-class");
+  });
+  removeButton.on("click", function () {
+    if (container.hasClass("new-class")) {
+      classDiv.removeClass("new-class");
+    }
+  });
+
+  // Exercise 4
+  fadeInButton.on("click", function () {
+    fadeDiv.fadeIn();
+  });
+  fadeOutButton.on("click", function () {
+    fadeDiv.fadeOut();
+  });
+
+  // Exercise 5
+  slideUpButton.on("click", function () {
+    slideDiv.slideUp();
+  });
+  slideDownButton.on("click", function () {
+    slideDiv.slideDown();
+  });
+});
